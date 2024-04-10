f304b467fc1c893af7c5f811341a86587645a7da
MeteorLee <meteorproject529@gmail.com>
2024-04-11 02:14:25 +0900

    #10 - repository interface 작성
    이걸로 db에 접근

---
+++ src/main/java/com/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.projectboard.repository;
+
+import com.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
