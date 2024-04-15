0a41bf9dd72e8ad16bf3bf937e68583e32e8724b
MeteorLee <meteorproject529@gmail.com>
2024-04-15 12:38:17 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExcutor`,
    `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- src/main/java/com/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package com.projectboard.repository;
 
 import com.projectboard.domain.ArticleComment;
+import com.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,  // 기본 검색 기능 추가
+        QuerydslBinderCustomizer<QArticleComment> { // 검색 기능 설정 (부분 검색)
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content,root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
