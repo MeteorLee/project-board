276ee6943aadf4cd2295ea69d1d9cd3448de9441
MeteorLee <meteorproject529@gmail.com>
2024-10-11 01:47:41 +0900

    #22 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- src/main/java/com/projectboard/domain/ArticleComment.java
+++ src/main/java/com/projectboard/domain/ArticleComment.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true) // 상위 클래스 toString
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -28,6 +28,7 @@ public class ArticleComment extends AuditingFields{
     private Long id;
 
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
 //    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
@@ -37,13 +38,14 @@ public class ArticleComment extends AuditingFields{
 
     protected ArticleComment() {}
 
-    public ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    private static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
